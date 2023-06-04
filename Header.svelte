7086d43b5e2c85cf7da0d6a34ef13e13bd810cb3
tstammann <115533860+tstammann@users.noreply.github.com>
2023-06-04 12:41:08 +0200

    Add files via upload

---
+++ Header.svelte
@@ -0,0 +1,23 @@
+<script>
+	
+</script>
+<div class="background">
+    <h1> hello</h1>
+    <div class="links">
+        <p> link zu "über mich"</p>
+    </div>
+    <h1 style="color:black;"> bye</h1>
+</div>
+
+<style>
+    .background{
+        background-color: #377691;
+    }
+    h1{
+        color: white;
+    }
+    p{
+        text-decoration: underline;
+    }
+	
+</style>    
